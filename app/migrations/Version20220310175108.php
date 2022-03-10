3528cc6fca3b02da61fd4826f24f1003b1baaee4
Fabien <fabien.vincent@me.com>
2022-03-10 18:52:04 +0100

    regroup migrations

--- app/migrations/Version20220310175108.php
+++ app/migrations/Version20220310175108.php
@@ -10,18 +10,19 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220302223326 extends AbstractMigration
+final class Version20220310175108 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Init DB';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, tag_id INT NOT NULL, reserved_by_id INT DEFAULT NULL, urls LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, cover_filename VARCHAR(255) DEFAULT NULL, price DOUBLE PRECISION NOT NULL, reserved_text LONGTEXT DEFAULT NULL, status VARCHAR(255) NOT NULL, INDEX IDX_23A0E66BAD26311 (tag_id), INDEX IDX_23A0E66BCDB4AF4 (reserved_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, tag_id INT NOT NULL, reserved_by_id INT DEFAULT NULL, urls LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, cover_filename VARCHAR(255) DEFAULT NULL, price DOUBLE PRECISION NOT NULL, reserved_text LONGTEXT DEFAULT NULL, status VARCHAR(255) NOT NULL, reserved_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_23A0E66BAD26311 (tag_id), INDEX IDX_23A0E66BCDB4AF4 (reserved_by_id), FULLTEXT INDEX IDX_23A0E662B36786B6DE44026 (title, description), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, label VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, position INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE texte (id INT AUTO_INCREMENT NOT NULL, slot VARCHAR(50) NOT NULL, message LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, firstname VARCHAR(100) NOT NULL, lastname VARCHAR(100) NOT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E66BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id)');
@@ -35,6 +36,7 @@ final class Version20220302223326 extends AbstractMigration
         $this->addSql('ALTER TABLE article DROP FOREIGN KEY FK_23A0E66BCDB4AF4');
         $this->addSql('DROP TABLE article');
         $this->addSql('DROP TABLE tag');
+        $this->addSql('DROP TABLE texte');
         $this->addSql('DROP TABLE `user`');
         $this->addSql('DROP TABLE messenger_messages');
     }
