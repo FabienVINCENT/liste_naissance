25c9aebf38a08d08a17bd3fe722d57a869445962
Fabien <fabien.vincent@me.com>
2022-03-21 17:46:06 +0100

    Add admin

--- app/src/Controller/Admin/UserController.php
+++ app/src/Controller/Admin/UserController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,5 +20,22 @@ class UserController extends AbstractController
         return $this->render('admin/user/index.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
+    }
+
+    #[Route('/admin/user/{id}', name: 'admin_user_toggle_role')]
+    public function toggleRoleAdmin(User $user, EntityManagerInterface $em): RedirectResponse
+    {
+        $roles = $user->getRoles();
+        if ($user->getId() !== $this->getUser()->getId()) {
+            if (($key = array_search('ROLE_ADMIN', $roles, true)) !== false) {
+                unset($roles[$key]);
+            } else {
+                $roles[] = 'ROLE_ADMIN';
+            }
+            $user->setRoles($roles);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_user_index');
     }
 }
