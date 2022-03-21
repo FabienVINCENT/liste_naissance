492966d795ac349e461480ac54c7da68b7eb5319
Fabien <fabien.vincent@me.com>
2022-03-21 09:03:44 +0100

    Update UserController.php
    update security

--- app/src/Controller/Admin/UserController.php
+++ app/src/Controller/Admin/UserController.php
@@ -4,9 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_ADMIN')]
 class UserController extends AbstractController
 {
     #[Route('/admin/user', name: 'admin_user_index')]
